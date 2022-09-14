392dcec81ad6426253c5ca0f2628a36916f5a2fa
bxits <71964606+bxits@users.noreply.github.com>
2022-09-14 12:56:21 +0200

    VS-Projekt eingefügt

---
+++ Fitnesstrainer/Fitnesstrainer/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommandHelper;
+
+namespace Fitnesstrainer.ViewModel
+{
+    class MainViewModel
+    {
+        private string _nameTrainingseinheit; //Feld
+        private ICommand _speichernCommand;
+        
+
+        public string NameTrainingseinheit
+        {
+            get { return _nameTrainingseinheit; }
+            set { _nameTrainingseinheit = value; }
+        }
+
+        public ICommand SpeichernCommand
+        {
+            get
+            {
+                if (_speichernCommand == null)
+                    _speichernCommand = new RelayCommand(c => SpeichereTrainingseinheit());
+                return _speichernCommand;
+            }
+        }
+
+        
+        public MainViewModel()
+        {
+
+        }
+
+        private void SpeichereTrainingseinheit()
+        {
+            
+        }
+    }
+}
